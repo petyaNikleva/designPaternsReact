22effae57a4e559085d9892d5e3bdee737bc269f
P N <petyanikleva@yahoo.com>
2024-02-28 17:41:13 +0200

    resursive components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecursiveComponent } from "./components/RecursiveComponent.jsx";
 
-function App() {
-
-  return (
-    <>
-      Hi
-    </>
-  )
+const nestedObject = {
+	a: 1,
+	b: {
+		b1: 4,
+		b2: {
+			b23: 'Hello',
+		},
+		b3: {
+			b31: {
+				message: 'Hi',
+			},
+			b32: {
+				message: 'Hi',
+			}
+		}
+	},
+	c: {
+		c1: 2,
+		c2: 3,
+	}
 }
 
-export default App
+function App() {
+	return (
+		<>
+	    <RecursiveComponent data={nestedObject} />
+		</>
+	);
+}
+
+export default App;
